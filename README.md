4ecf7eda320804018499ceddc37f3b4aaecdd702
sitek94 <msitkowski94@gmail.com>
2021-05-23 12:17:17 +0200

    add some comments

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 Learning by building from scratch, breaking stuff and destructuring. 
 
+## Inspired by
+
 * [Deconstructing React by Tejas Kumar](https://www.youtube.com/watch?v=f2mMOiCSj5c&t=659s)
+* [Inside React: A Deeper Look by Tejas Kumar](https://www.youtube.com/watch?v=EonphfGhGgI)
+
+## Install
+
+```bash
+npm i
+npm start
+```
--- src/react.ts
+++ src/react.ts
@@ -4,6 +4,9 @@ export function createElement(type, props, ...children) {
   // It's a component!
   if (typeof type === 'function') {
     const Component = type;
+
+    // Return the result of calling the component with props
+    // React Component - a function that returns JSX
     return Component(props);
   }
 
@@ -24,14 +27,20 @@ let stateIndex = 0;
 export function useState<S>(initialState): [S, (S) => void] {
   const currentIndex = stateIndex;
 
+  // Use initial state only if there is no state yet
   states[currentIndex] = states[currentIndex] || initialState;
 
   const setState = newState => {
     states[currentIndex] = newState;
+
+    // Rerender the component when state has changed and set stateIndex back to 0.
     rerender(() => (stateIndex = 0));
   };
 
+  // Increment stateIndex so that next call to useState will be responsible for 
+  // next piece of state
   stateIndex++;
 
+  // Return state, and setter
   return [states[currentIndex], setState];
 }
